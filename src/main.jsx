6238bb9bc1bbc5bcfdd85256451cdce5dae41b8f
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-10-17 17:57:25 -0300

    testes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-dom/client'
+import Home from './routes/Cabecalho/index'
+import Home from './routes/Error/index'
+import Home from './routes/Home/index'
+import Home from './routes/Rodape/index'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement
+  }
+])
