13e601a94ff58a452fd72cae89af20da79360fca
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-10-18 14:32:16 -0300

    email e telefone do cabeçalho

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
 
 export default function Cabecalho() {
     return (
         <>
-            <div>
-                
+            <div className='cabe1'>
+                <div className='cabe1-infos'>
+                    <div className='cabe1-telefone'>
+                        <img src="/images/telefone.png" alt="Telefone" />
+                        <p>+55 (11) 3384-8550</p>
+                    </div>
+
+                    <div className='cabe1-email'>
+                        <img src="/images/email.png" alt="Email" />
+                        <p>atendimento@aliancatraducoes.com.br</p>
+                    </div>
+                </div>
+
+                <div className='cabe1-links'>
+
+                </div>
             </div>
         </>
     )
